2518a08edfcedf82106add7c9ee5659d27ae8906
Christian Gawron <gawron.christian@fh-swf.de>
2025-01-17 15:23:59 +0000

    feat: party updates as of 2025-01-17
    - added "Die Linke" program
    - added "SPD Regierungsprogramm"
    - added "Volt" program

--- PartyRetriever.py
+++ PartyRetriever.py
@@ -8,21 +8,19 @@ from langchain_core.embeddings.embeddings import Embeddings
 from langchain_core.documents.base import Document
 
 
-
-
-
 class PartyRetriever(BaseRetriever):
     """
     A retriever that retrieves documents from the party programs"""
-    
+
     docs: Mapping[str, str] = {
-        "BSW": "BSW_Parteiprogramm.pdf",
+        "BSW": "BSW_Wahlprogramm_2025__Entwurf_.pdf",
         "Grüne": "Grüne_BTW2025.pdf",
         "CDU": "CDU_BTW2025.pdf",
-        "AfD": "Programm_AfD_Online_.pdf",
-        "Linke": "DIE_LINKE_Wahlprogramm_zur_Bundestagswahl_2021.pdf",
-        "SPD": "SPD-Zukunftsprogramm.pdf",
-        "FDP": "FDP_Programm_Bundestagswahl2021_1.pdf"
+        "AfD": "AfD_Leitantrag-Bundestagswahlprogramm-2025.pdf",
+        "Linke": "btw_2025_wahlprogramm_die_linke.pdf",
+        "SPD": "BTW_2025_SPD_Regierungsprogramm.pdf",
+        "FDP": "fdp-wahlprogramm_2025.pdf",
+        "Volt": "volt-programm-bundestagswahl-2025.pdf"
     }
     vectorstore: VectorStore
     embeddings: Embeddings
@@ -41,6 +39,7 @@ class PartyRetriever(BaseRetriever):
         results = []
         query_embedding = self.embeddings.embed_query(query)
         for party in self.docs:
-            results += self.vectorstore.similarity_search_by_vector(query_embedding, k=3, filter={'party': party})
+            results += self.vectorstore.similarity_search_by_vector(
+                query_embedding, k=3, filter={'party': party})
 
         return results
